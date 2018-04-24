1d021bcfe99f9c22a6e6d288891b15e12bc2b713
Alex Buchanan <buchanae@ohsu.edu>
2018-04-24 16:15:00 -0700

    clean up option parsing and regulon save/load

--- run_arachne2regulon.R
+++ run_arachne2regulon.R
@@ -1,61 +1,44 @@
-
-#example of optparse usage https://gist.github.com/ericminikel/8428297
 suppressPackageStartupMessages(require(optparse))
 suppressPackageStartupMessages(require(Biobase))
 suppressPackageStartupMessages(require(viper))
 
-# function that loads expression matrix of unzipped or gzipped file
-load_expr <- function(expr_file) {
-	if (endsWith(expr_file, '.gz')) {
-		expr <- as.matrix(read.table(gzfile(expr_file, 'rt'), row.names=1, sep='\t',
-							check.names=FALSE, header=TRUE))
-	} else {
-		expr <- as.matrix(read.table(expr_file, row.names=1, sep='\t',
-							check.names=FALSE, header=TRUE))
-	}
-	
-	return(expr)
+opt <- OptionParser(option_list=list(
+  make_option(c("-o", "--outdir"), action="store", type='character',
+        help='Path to output directory.'),
+  make_option(c("-e", "--expr"), action="store", type='character',
+        help='Path to input expression matrix used to build ARACNE network.'),
+  make_option(c("-n", "--network"), action="store", type='character',
+        help='Path to ARACNE network.txt.')
+))
+
+opt <- parse_args(parser)
+outdir <- opt$o
+expr_fl <- opt$e
+network_fl <- opt$n
+
+if (is.null(outdir)) {
+  print_help(parser)
+  stop("--outdir is required")
 }
 
-main <- function() {
-	
-	# allow for commandline args
-	option_list = list(
-	make_option(c("-d", "--dir"), action="store", default=NA, type='character',
-				help='Full path to outdir.'),
-	make_option(c("-e", "--expr"), action="store", default=NA, type='character',
-				help='Full path to input expression matrix used to build ARACNE network.'),
-	make_option(c("-n", "--network"), action="store", default=NA, type='character',
-				help='Full path to ARACNE network.txt.')
-	)
-	
-	opt = parse_args(OptionParser(option_list=option_list))
-	
-	# todo: make sure this works
-	# todo: report which isn't working and exit
-	if(any(is.na(c(opt$d, opt$e, opt$n)))) {
-		cat("One or more variables have not been set!")
-	}
-	
-	outdir=opt$d
-	expr_aracne_fl=opt$e
-	network_fl=opt$n
-	
-	# print session info
-	writeLines(capture.output(sessionInfo()), paste(outdir, "RsessionInfo.txt", sep=""))
-	
-	print("Loading expression data from which ARACNE network was derived in R.")
-	expr_a <- load_expr(expr_aracne_fl)
-								 
-	print("Loading ARACNE network in R and converting to regulon obj")
-	regul <- aracne2regulon(network_fl, expr_a)
-	
-	#print("Writing out activity scores")
-  save(regul, file=paste(outdir, 'regulons.tsv', sep=''))
-	
+if (is.null(expr_fl)) {
+  print_help(parser)
+  stop("--expr is required")
 }
 
+if (is.null(network_fl)) {
+  print_help(parser)
+  stop("--network is required")
+}
 
-main()
+# print session info
+writeLines(capture.output(sessionInfo()), file.path(outdir, "RsessionInfo.txt"))
 
+cat("Loading expression data from which ARACNE network was derived in R.")
+expr_dt <- read.table(expr_fl, row.names=1, sep='\t', check.names=FALSE, header=TRUE)
+expr_a <- as.matrix(expr_dt)
+               
+cat("Loading ARACNE network in R and converting to regulon obj")
+regul <- aracne2regulon(network_fl, expr_a)
 
+saveRDS(regul, file=file.path(outdir, 'regulons.rds'))
---
+++ run_viper.R
@@ -0,0 +1,50 @@
+suppressPackageStartupMessages(require(optparse))
+suppressPackageStartupMessages(library(Biobase))
+suppressPackageStartupMessages(library(viper))
+
+
+parser <- OptionParser(option_list=list(
+  make_option(c("-o", "--outdir"), action="store", type='character',
+        help='Path to output directory.'),
+
+  make_option(c("-r", "--regulons"), action="store", type='character',
+        help='Path to RDS file containing network regulons.'),
+        
+  make_option(c("-e", "--expr"), action="store", type='character',
+        help='Path to file containing expression of samples for which viper\n
+        activity scores are desired.')		
+))
+
+opt <- parse_args(parser)
+outdir <- opt$o
+reg_fl <- opt$r
+expr_fl <- opt$e
+
+if (is.null(outdir)) {
+  print_help(parser)
+  stop("--outdir is required")
+}
+if (is.null(reg_fl)) {
+  print_help(parser)
+  stop("--regulons is required")
+}
+if (is.null(expr_fl)) {
+  print_help(parser)
+  stop("--expr is required")
+}
+
+# print session info
+writeLines(capture.output(sessionInfo()), file.path(outdir, "RsessionInfo.txt"))
+               
+cat("Loading network regulons.")
+regul <- readRDS(reg_fl)
+
+cat("Loading expression data.")
+expr_dt <- read.table(expr_fl, row.names=1, sep='\t', check.names=FALSE, header=TRUE)
+expr_s <- as.matrix(expr_dt)
+
+cat("Running VIPER")
+vpres <- viper(expr_s, regul, verbose=TRUE)
+
+cat("Writing out activity scores")
+write.table(vpres, file=file.path(outdir, 'viper_activities.tsv'), sep='\t', quote=FALSE)
