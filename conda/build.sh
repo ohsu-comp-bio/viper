86db49bab6790d86be158f7c697e85f411f9501f
Alex Buchanan <buchanae@ohsu.edu>
2018-04-30 12:09:38 -0700

    move conda build scripts to root

--- conda/build.sh
+++
@@ -1,5 +0,0 @@
-# Conda package build script
-mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/../run_aracne2regulon.r $PREFIX/bin
-cp $RECIPE_DIR/../run_viper.r $PREFIX/bin
-cp $RECIPE_DIR/../viper_report.r $PREFIX/bin
