6a2eb0925583e6578eb62d0aa530b1b5070bb1be
Alex Buchanan <buchanae@ohsu.edu>
2018-04-30 11:55:01 -0700

    add conda build script and metadata

---
+++ conda/build.sh
@@ -0,0 +1,5 @@
+# Conda package build script
+mkdir -p $PREFIX/bin
+cp $RECIPE_DIR/../run_aracne2regulon.r $PREFIX/bin
+cp $RECIPE_DIR/../run_viper.r $PREFIX/bin
+cp $RECIPE_DIR/../viper_report.r $PREFIX/bin
